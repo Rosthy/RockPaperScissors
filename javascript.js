15ad18194b4929c339729403eedd3947687aa02d
Jonathas <jonathasdasilvacordeiro@gmail.com>
2023-04-08 23:54:18 -0300

    mostly finished

--- javascript.js
+++ javascript.js
@@ -13,20 +13,47 @@ function playRound(playerSelection,computerSelection){
     computerSelection = computerChoice()
     playerSelection = prompt("Take your pick").toLowerCase()
     if (computerSelection == playerSelection){
-        return "Tie round"
+        console.log("Tie round")
     }
     else if(computerSelection == "rock" && playerSelection == "scissors" ||
             computerSelection == "paper" && playerSelection == "rock" ||
             computerSelection == "scissors" && playerSelection == "paper"){
-        computerScore = ++computerScore;
-        return "You lose"
+            computerScore = ++computerScore;
+            console.log("Computer chose " + computerSelection)
+            console.log("You chose " + playerSelection )
+            console.log(computerSelection+" beats " + playerSelection)
+            console.log("You lose this round")
+
+
+        
     }
     else {
-        return "You win"
+        console.log("Computer chose " + computerSelection)
+        console.log("You chose " + playerSelection )
+        console.log(playerSelection+" beats " + computerSelection)
+        console.log("You win this round")
         playerScore = ++playerScore;
+        
     }
 
 }
-console.log(playRound())
-console.log(computerScore)
-console.log(playerScore)
+function game(){
+    while (true){
+        playRound()
+        if(playerScore == 5 || computerScore == 5){
+            break
+        }
+    }
+    if (computerScore > playerScore){
+        console.log("Player Score:" + playerScore)
+        console.log("Computer Score:" + computerScore)
+        return "Computer wins"
+    }
+    else{
+        console.log("Player Score:" + playerScore)
+        console.log("Computer Score:" + computerScore)
+        return "player wins"
+    }
+
+}
+console.log(game())
